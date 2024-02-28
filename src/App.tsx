cea6b8a60cce111a75ffd9628321fd4c66c3132c
maceq687 <maciek@softman.pl>
2024-02-28 13:04:11 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [state, setState] = useState("");
+
+  const updateHandle = (event) => {
+    setState(event.target.value);
+  };
+
+  function calculate() {
+    setState((prevState) => eval(prevState));
+  }
+
+  return (
+    <>
+      <div className="card">
+        <input type="text" value={state} onChange={updateHandle} />
+        <br />
+        <button onClick={calculate}>calculate</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
