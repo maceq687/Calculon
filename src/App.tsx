6bdbf35e19c52832d76c1883c477619c628ad111
maceq687 <maciek@softman.pl>
2024-02-28 15:10:05 +0100

    Added simple restriction and missing buttons

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,33 @@ function App() {
     setState(event.target.value);
   };
 
+  const textarea = document.getElementById("formula");
+
+  textarea?.addEventListener("keydown", (event) => {
+    if (
+      [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "+",
+        "-",
+        "*",
+        "/",
+        ".",
+        "Backspace",
+      ].indexOf(event.key) == -1
+    ) {
+      event.preventDefault();
+    }
+  });
+
   const addToFormula = (
     _event: React.MouseEvent<HTMLElement>,
     value: string
@@ -23,7 +50,7 @@ function App() {
   return (
     <>
       <div className="card">
-        <input type="text" value={state} onChange={updateHandle} />
+        <input type="text" id="formula" value={state} onChange={updateHandle} />
         <br />
         <CalcButton addToFormula={addToFormula} value="7" />
         <CalcButton addToFormula={addToFormula} value="8" />
@@ -40,8 +67,9 @@ function App() {
         <CalcButton addToFormula={addToFormula} value="3" />
         <CalcButton addToFormula={addToFormula} value="*" />
         <br />
-        <CalcButton addToFormula={addToFormula} value="," />
-        <button onClick={calculate}>calculate</button>
+        <CalcButton addToFormula={addToFormula} value="0" />
+        <CalcButton addToFormula={addToFormula} value="." />
+        <button onClick={calculate}>=</button>
         <CalcButton addToFormula={addToFormula} value="/" />
       </div>
     </>
