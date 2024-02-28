4b56c8a973a7835c9d964164e6fbc59f6f8258e4
maceq687 <maciek@softman.pl>
2024-02-28 23:03:04 +0100

    replaced restriction with regexr

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,18 @@ import CalcButton from "./components/CalcButton";
 
 function App() {
   const [state, setState] = useState("");
+  const re = /^-?(0|[1-9]\d*)(\.\d*)?([+\-*/]((0|[1-9]\d*)(\.\d*)?)?)*$/;
 
   const updateHandle = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setState(event.target.value);
+    if (re.test(event.target.value)) setState(event.target.value);
   };
-
-  const textarea = document.getElementById("formula");
-
-  textarea?.addEventListener("keydown", (event) => {
-    if (
-      [
-        "0",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "+",
-        "-",
-        "*",
-        "/",
-        ".",
-        "Backspace",
-      ].indexOf(event.key) == -1
-    ) {
-      event.preventDefault();
-    }
-  });
 
   const addToFormula = (
     _event: React.MouseEvent<HTMLElement>,
     value: string
   ) => {
-    setState((prevState) => prevState.concat(value));
+    if (re.test(state.concat(value)))
+      setState((prevState) => prevState.concat(value));
   };
 
   function calculate() {
@@ -50,7 +25,7 @@ function App() {
   return (
     <>
       <div className="card">
-        <input type="text" id="formula" value={state} onChange={updateHandle} />
+        <input type="text" value={state} onChange={updateHandle} />
         <br />
         <CalcButton addToFormula={addToFormula} value="7" />
         <CalcButton addToFormula={addToFormula} value="8" />
