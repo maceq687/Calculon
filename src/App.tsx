5aa203355a93039ec8bdba508de996173eebfb31
maceq687 <maciek@softman.pl>
2024-02-28 14:41:45 +0100

    added calcButton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import CalcButton from "./components/CalcButton";
 
 function App() {
   const [state, setState] = useState("");
 
   const updateHandle = (event: React.ChangeEvent<HTMLInputElement>) => {
     setState(event.target.value);
+  };
+
+  const addToFormula = (
+    _event: React.MouseEvent<HTMLElement>,
+    value: string
+  ) => {
+    setState((prevState) => prevState.concat(value));
   };
 
   function calculate() {
@@ -17,7 +25,24 @@ function App() {
       <div className="card">
         <input type="text" value={state} onChange={updateHandle} />
         <br />
+        <CalcButton addToFormula={addToFormula} value="7" />
+        <CalcButton addToFormula={addToFormula} value="8" />
+        <CalcButton addToFormula={addToFormula} value="9" />
+        <CalcButton addToFormula={addToFormula} value="+" />
+        <br />
+        <CalcButton addToFormula={addToFormula} value="4" />
+        <CalcButton addToFormula={addToFormula} value="5" />
+        <CalcButton addToFormula={addToFormula} value="6" />
+        <CalcButton addToFormula={addToFormula} value="-" />
+        <br />
+        <CalcButton addToFormula={addToFormula} value="1" />
+        <CalcButton addToFormula={addToFormula} value="2" />
+        <CalcButton addToFormula={addToFormula} value="3" />
+        <CalcButton addToFormula={addToFormula} value="*" />
+        <br />
+        <CalcButton addToFormula={addToFormula} value="," />
         <button onClick={calculate}>calculate</button>
+        <CalcButton addToFormula={addToFormula} value="/" />
       </div>
     </>
   );
---
+++ src/components/CalcButton.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  value: string;
+  addToFormula: (event: React.MouseEvent<HTMLElement>, value: string) => void;
+};
+
+function CalcButton({ value, addToFormula }: Props) {
+  return (
+    <>
+      <button onClick={(event) => addToFormula(event, value)}>{value}</button>
+    </>
+  );
+}
+
+export default CalcButton;
